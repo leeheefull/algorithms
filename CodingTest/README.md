2480ffd521b1486749ff4daeb78ad9340eb41706
leeheefull <coolzz7@kakao.com>
2020-08-18 13:55:29 +0900

    CodingTest: make Omok

--- CodingTest/README.md
+++ CodingTest/README.md
@@ -1,11 +1,11 @@
-# [Coding-Test](https://github.com/leeheefull/Algorithms/tree/master/CodingTest)
-
+# Coding-Test
 > Algorithm problem practice using 'Java'
 
 <details>
   <summary>Loop</summary>
   
   ---
-  ##### [Pyramid](https://github.com/leeheefull/Algorithms/tree/master/CodingTest/src/loop/Pyramid.java)
+  ##### [> Pyramid](https://github.com/leeheefull/Algorithms/tree/master/CodingTest/src/loop/Pyramid.java)
+  ##### [> Omok](https://github.com/leeheefull/Algorithms/tree/master/CodingTest/src/loop/Omok.java)
   ---
 </details>
---
+++ CodingTest/src/loop/Omok.java
@@ -0,0 +1,65 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Omok {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("* width: ");
+			int w = scanner.nextInt();
+		System.out.print("* height: ");
+			int h = scanner.nextInt();
+		System.out.print("* point x: ");
+			int a = scanner.nextInt();
+		System.out.print("* point y: ");
+			int b = scanner.nextInt();
+			
+		for (int y=1; y<=h; y++) {
+			for (int x=1; x<=w; x++) {
+				if(x==a && y==b)
+					System.out.printf("%c", '¡Ü');
+				else if(x==1 && y==1)
+					System.out.printf("%c", '¦£');
+				else if(x==w && y==1)
+					System.out.printf("%c", '¦¤');
+				else if(x==1 && y==h)
+					System.out.printf("%c", '¦¦');
+				else if(x==w && y==h)
+					System.out.printf("%c", '¦¥');
+				else if(x==1)
+					System.out.printf("%c", '¦§');
+				else if(y==1)
+					System.out.printf("%c", '¦¨');
+				else if(x==w)
+					System.out.printf("%c", '¦©');
+				else if(y==h)
+					System.out.printf("%c", '¦ª');
+				else
+					System.out.printf("%c", '¦«');
+			}
+			System.out.println();
+		}
+		scanner.close();
+	}
+}
+
+// output
+/*********************
+* width: 20
+* height: 10
+* point x: 4
+* point y: 3
+¦£¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¨¦¤
+¦§¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦©
+¦§¦«¦«¡Ü¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦©
+¦§¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦©
+¦§¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦©
+¦§¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦©
+¦§¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦©
+¦§¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦©
+¦§¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦«¦©
+¦¦¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦ª¦¥
+*********************/
