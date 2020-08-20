28a5e6962717dc4bfa271ed2d91c4fc203a1847b
leeheefull <coolzz7@kakao.com>
2020-08-20 15:26:41 +0900

    concept: sort: implemented bubble sort

---
+++ Concept/src/sort/Bubble.java
@@ -0,0 +1,41 @@
+package sort;
+
+public class Bubble {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {30, 60, 40, 50, 20, 10};  
+
+		// after
+		for(int i=0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+
+		System.out.println();
+		bubbleSort(arr);
+		
+		// before
+		for(int i=0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+	}
+	
+	public static void bubbleSort(int[] arr) {
+		
+		int n = arr.length;
+		for(int i=n-1; 0<i; i--)
+			for(int j=0; j<i; j++)
+				if(arr[j] > arr[j+1])    
+					swap(j, j+1, arr);
+	}
+	
+	public static void swap(int x, int y, int[] arr) {
+		int tmp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = tmp;
+	}
+}
+
+// output
+/*****************
+30 60 40 50 20 10 
+10 20 30 40 50 60 
+******************/
