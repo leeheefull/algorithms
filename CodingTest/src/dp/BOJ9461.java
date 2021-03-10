db92856bf69495b0a67d898f780a68689f53607f
leeheefull <coolzz7@kakao.com>
2021-03-10 10:16:33 +0900

    Update BOJ9461(dp)

---
+++ CodingTest/src/dp/BOJ9461.java
@@ -0,0 +1,66 @@
+/*
+파도반 수열 성공출처다국어분류
+시간 제한	메모리 제한	제출      정답	맞은 사람	정답 비율
+1 초	    128 MB  	42747	 17868	14731   	40.423%
+문제
+오른쪽 그림과 같이 삼각형이 나선 모양으로 놓여져 있다.
+첫 삼각형은 정삼각형으로 변의 길이는 1이다.
+그 다음에는 다음과 같은 과정으로 정삼각형을 계속 추가한다.
+나선에서 가장 긴 변의 길이를 k라 했을 때, 그 변에 길이가 k인 정삼각형을 추가한다.
+
+파도반 수열 P(N)은 나선에 있는 정삼각형의 변의 길이이다.
+P(1)부터 P(10)까지 첫 10개 숫자는 1, 1, 1, 2, 2, 3, 4, 5, 7, 9이다.
+
+N이 주어졌을 때, P(N)을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있고, N이 주어진다. (1 ≤ N ≤ 100)
+
+출력
+각 테스트 케이스마다 P(N)을 출력한다.
+
+예제 입력 1
+2
+6
+12
+예제 출력 1
+3
+16
+ */
+
+package dp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ9461 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine()); // test case
+        long[] p = new long[101];
+
+        for (int i = 0; i < p.length; i++) {
+            if (i == 0) {
+                p[i] = 0;
+            } else if (i <= 3) {
+                p[i] = 1;
+            } else if (i == 4) {
+                p[i] = 2;
+            } else {
+                p[i] = p[i - 1] + p[i - 5];
+            }
+        }
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            bw.write(p[n] + "\n");
+        }
+        bw.flush();
+    }
+}
