6d24521346dd3644d99f1ab933138fd09fa12f1f
leeheefull <coolzz7@kakao.com>
2021-04-08 14:33:48 +0900

    Solve BOJ1715

---
+++ CodingTest/src/greedy/BOJ1715.java
@@ -0,0 +1,30 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ1715 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+        long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+        while (1 < pq.size()) {
+            int a = pq.poll();
+            int b = pq.poll();
+
+            result += a + b;
+            pq.add(a + b);
+        }
+        System.out.print(result);
+    }
+}
