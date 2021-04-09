c0b390a1ebda07a8b286d4c319085059939cb86a
leeheefull <coolzz7@kakao.com>
2021-04-09 15:43:08 +0900

    Solve BOJ14501

---
+++ CodingTest/src/dp/BOJ14501.java
@@ -0,0 +1,32 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ14501 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] t = new int[n + 2];
+        int[] p = new int[n + 2];
+        int[] dp = new int[n + 6];
+
+        for (int i = 1; i < n + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < n + 2; i++) {
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            dp[t[i] + i] = Math.max(dp[t[i] + i], dp[i] + p[i]);
+        }
+        System.out.print(dp[n + 1]);
+    }
+}
