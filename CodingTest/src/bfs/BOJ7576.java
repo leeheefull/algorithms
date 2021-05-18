42a48e20520d72d47829531cbb06c92e85409785
leeheechan <coolzz7@naver.com>
2021-05-18 14:40:21 +0900

    Solve BOJ7576

---
+++ CodingTest/src/bfs/BOJ7576.java
@@ -0,0 +1,79 @@
+package bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ7576 {
+
+    private static int n, m;
+    private static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int result = bfs();
+        System.out.print(check() ? result : -1);
+    }
+
+    private static boolean check() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static int bfs() {
+        int result = 0;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+
+        Queue<Node> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 1) {
+                    queue.add(new Node(i, j));
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nextX = current.x + dx[i];
+                int nextY = current.y + dy[i];
+
+                if (0 <= nextX && 0 <= nextY && nextX < n && nextY < m
+                        && map[nextX][nextY] == 0) {
+                    map[nextX][nextY] += map[current.x][current.y] + 1;
+                    result = Math.max(result, map[nextX][nextY]);
+                    queue.add(new Node(nextX, nextY));
+                }
+            }
+        }
+
+        if (result == 0) {
+            return 0;
+        } else {
+            return result - 1;
+        }
+    }
+}
