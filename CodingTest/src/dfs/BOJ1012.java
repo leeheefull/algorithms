ddf38fbd5062819b29aa5ffbd0d878cd6f902896
leeheechan <coolzz7@naver.com>
2021-05-19 18:14:58 +0900

    Solve BOJ1012 using dfs

---
+++ CodingTest/src/dfs/BOJ1012.java
@@ -0,0 +1,63 @@
+package dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1012 {
+
+    private static int n, m;
+    private static boolean[][] map;
+    private static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int result = 0;
+            map = new boolean[n][m];
+            visited = new boolean[n][m];
+
+            for (int j = 0; j < k; j++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                map[y][x] = true;
+            }
+
+            for (int x = 0; x < n; x++) {
+                for (int y = 0; y < m; y++) {
+                    if (map[x][y] && !visited[x][y]) {
+                        result++;
+                        dfs(x, y);
+                    }
+                }
+            }
+            System.out.println(result);
+        }
+    }
+
+    private static void dfs(int x, int y) {
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+
+            if (0 <= nextX && 0 <= nextY && nextX < n && nextY < m
+                    && !visited[nextX][nextY] && map[nextX][nextY]) {
+                dfs(nextX, nextY);
+            }
+        }
+    }
+}
