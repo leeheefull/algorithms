8a2d1055405274dec83852efd0028eb4fc8c41d7
leeheechan <coolzz7@naver.com>
2021-07-06 00:03:39 +0900

    Solve thisIsCodingTest 모험가 길드

---
+++ src/main/java/thisIsCodingTest/chapter11/Question1.java
@@ -0,0 +1,21 @@
+package thisIsCodingTest.chapter11;
+
+import java.util.*;
+
+public class Question1 {    // 모험가 길드
+    public int solve(int n, List<Integer> fears) {
+        int answer = 0;
+        int cnt = 0;
+
+        Collections.sort(fears);
+
+        for (int v : fears) {
+            cnt++;
+            if (v <= cnt) {
+                cnt = 0;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
