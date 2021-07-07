be28847e14e6c95e5f1d39d99d4868a5e963c29b
leeheechan <coolzz7@naver.com>
2021-07-07 15:37:38 +0900

    Solve 볼링공 고르기

---
+++ src/main/java/thisIsCodingTest/chapter11/Question5.java
@@ -0,0 +1,30 @@
+package thisIsCodingTest.chapter11;
+
+public class Question5 {    // 볼링공 고르기
+    public int solve(int n, int maxWeight, int[] balls) {
+        int[] weights = new int[maxWeight + 1];
+        for (int ball : balls) {
+            weights[ball]++;
+        }
+
+        int answer = n * (n - 1) / 2;
+        for (int weight : weights) {
+            if (1 < weight) {
+                answer -= weight * (weight - 1) / 2;
+            }
+        }
+        return answer;
+    }
+
+//    public int solve(int n, int[] balls) {
+//        int answer = n * (n - 1) / 2;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = i + 1; j < n; j++) {
+//                if (balls[i] == balls[j]) {
+//                    answer--;
+//                }
+//            }
+//        }
+//        return answer;
+//    }
+}
---
+++ src/test/java/thisIsCodingTest/chapter11/Question5Test.java
@@ -0,0 +1,67 @@
+package thisIsCodingTest.chapter11;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Question5Test {
+    private Question5 q5;
+
+    @BeforeEach
+    void setUp() {
+        q5 = new Question5();
+    }
+
+    @Test
+    @DisplayName("5 / 3 / 1 3 2 3 2")
+    void test1() {
+        // given
+        int n = 5;
+        int maxWeight = 3;
+        int[] balls = {1, 3, 2, 3, 2};
+
+        // when
+        int actual = q5.solve(n, maxWeight, balls);
+        int expected = 8;
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("8 / 5 / 1 5 4 3 2 4 5 2")
+    void test2() {
+        // given
+        int n = 8;
+        int maxWeight = 5;
+        int[] balls = {1, 5, 4, 3, 2, 4, 5, 2};
+
+        // when
+        int actual = q5.solve(n, maxWeight, balls);
+        int expected = 25;
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("5 / 3 / 1 3 2 3 3")
+    void test3() {
+        // given
+        int n = 5;
+        int maxWeight = 3;
+        int[] balls = {1, 3, 2, 3, 3};
+
+        // when
+        int actual = q5.solve(n, maxWeight, balls);
+        int expected = 7;
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
