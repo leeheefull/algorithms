149eb11f22d6ab802e99e94ea4de2fafcef9a32e
leeheechan <coolzz7@naver.com>
2021-07-08 17:15:11 +0900

    Solve 시리얼 번호

--- src/main/java/baekjoonOnlineJudge/BOJ1431.java
+++ src/main/java/baekjoonOnlineJudge/BOJ1431.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class BOJ1432 {  // 시리얼 번호
+public class BOJ1431 {  // 시리얼 번호
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
