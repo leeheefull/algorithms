0702cd18ad4576e6985dfeb1fca4cf9d509440a6
leeheechan <coolzz7@naver.com>
2021-08-08 22:51:32 +0900

    Solve 큰 수의 법칙

---
+++ src/main/java/thisIsCodingTest/chapter03/Question2.java
@@ -0,0 +1,11 @@
+package thisIsCodingTest.chapter03;
+
+import java.util.Arrays;
+
+public class Question2 {    // 큰 수의 법칙
+    public int solve(int n, int m, int k, int[] nums) {
+        Arrays.sort(nums);
+        int cnt = (m / (k + 1)) * k + (m % (k + 1));
+        return cnt * nums[n - 1] + (m - cnt) * nums[n - 2];
+    }
+}
