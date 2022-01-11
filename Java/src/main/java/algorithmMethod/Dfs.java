297e27f26674a18ee7205280970d1db1bcc49877
leeheechan <leeheefull@gmail.com>
2022-01-11 12:31:31 +0900

    update dfs

--- Java/src/main/java/algorithmMethod/Dfs.java
+++ Java/src/main/java/algorithmMethod/Dfs.java
@@ -3,6 +3,11 @@ package algorithmMethod;
 public class Dfs {
     /**
      * map의 시작 점을 기준으로 1인 부분이 인접한 모든 곳을 0으로 변경한다.
+     *
+     * @param map 0과 1로 구성된 map, 1은 이동 가능한 공간
+     * @param x   위치 값으로써 행을 결정
+     * @param y   위치 값으로써 열을 결정
+     * @return 시작 점으로부터 방문한 공간을 1에서 0으로 바꾼 후 map을 반환
      */
     public int[][] dfs(int[][] map, int x, int y) {
         // 방문한 곳은 0으로 변경
