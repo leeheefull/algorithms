3b4ae07cb9b73a0582b1974c36314e819e48fb32
leeheechan <leeheefull@gmail.com>
2022-01-21 14:43:59 +0900

    solve 오픈채팅방

---
+++ Java/src/test/java/programmers/오픈채팅방Test.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class 오픈채팅방Test {
+    private 오픈채팅방 test;
+
+    @BeforeEach
+    void setUp() {
+        test = new 오픈채팅방();
+    }
+
+    @Test
+    void test1() {
+        // given
+        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
+
+        // when
+        String[] actual = test.solution(record);
+        String[] expected = {"Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."};
+
+        // then
+        assertArrayEquals(expected, actual);
+    }
+}
