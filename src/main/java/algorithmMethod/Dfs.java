543e2a317e108a0e0c17bcb0e88a03e3301d5cbe
leeheechan <leeheefull@gmail.com>
2022-03-22 13:39:43 +0900

    update dfs

--- src/main/java/algorithmMethod/Dfs.java
+++ src/main/java/algorithmMethod/Dfs.java
@@ -1,15 +1,27 @@
 package algorithmMethod;
 
 public class Dfs {
+    private final int[][] map;
+
+    /**
+     * 0은 벽이고, 1은 길이다.
+     */
+    public Dfs(int[][] map) {
+        this.map = map;
+    }
+
     /**
      * map의 시작 점을 기준으로 1인 부분이 인접한 모든 곳을 0으로 변경한다.
      *
-     * @param map 0과 1로 구성된 map, 1은 이동 가능한 공간
-     * @param x   위치 값으로써 행을 결정
-     * @param y   위치 값으로써 열을 결정
+     * @param x 위치 값으로써 행을 결정
+     * @param y 위치 값으로써 열을 결정
      * @return 시작 점으로부터 방문한 공간을 1에서 0으로 바꾼 후 map을 반환
      */
-    public int[][] dfs(int[][] map, int x, int y) {
+    public int[][] spread(int x, int y) {
+        return execute(map, x, y);
+    }
+
+    private int[][] execute(int[][] map, int x, int y) {
         // 방문한 곳은 0으로 변경
         map[x][y]--;
 
@@ -27,7 +39,7 @@ public class Dfs {
                     0 <= nx && nx < map.length &&
                     map[nx][ny] == 1
             ) {
-                dfs(map, nx, ny);
+                execute(map, nx, ny);
             }
         }
         return map;
--- src/test/java/algorithmMethod/DfsTest.java
+++ src/test/java/algorithmMethod/DfsTest.java
@@ -1,17 +1,11 @@
 package algorithmMethod;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class DfsTest {
     private Dfs dfs;
-
-    @BeforeEach
-    void setUp() {
-        this.dfs = new Dfs();
-    }
 
     @Test
     void dfs() {
@@ -26,9 +20,10 @@ class DfsTest {
                 {0, 1, 1, 1, 0, 0, 0},
                 {0, 1, 1, 1, 0, 0, 0}
         };
+        this.dfs = new Dfs(map);
 
         // when
-        int[][] actual = dfs.dfs(map, 0, 1);
+        int[][] actual = dfs.spread(0, 1);
         int[][] expected = {
                 {0, 0, 0, 0, 1, 0, 0},
                 {0, 0, 0, 0, 1, 0, 1},
